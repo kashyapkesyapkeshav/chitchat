c2176a2df5ec7d487593e8373dad577e12ac1596
keshav <kashyapkeshav@gmail.com>
2025-06-07 10:35:57 +0530

    Add files via upload

---
+++ basic_math_opeartions.py
@@ -0,0 +1,7 @@
+num_1=int(input("enter the first number\n"))
+num_2=int(input("enter the Second number\n"))
+
+print("Addition : ",num_1+num_2)
+print("subtraction : ",num_1-num_2)
+print("multiplication",num_1*num_2)
+print("Division : ",num_1/num_2)
---
+++ personnel Greeting.py
@@ -0,0 +1,3 @@
+firstname=input("enter the firstname\n ")
+lastname=input("enter the lastname\n")
+print("Hello," +firstname+ " " + lastname+"!"+"we   lcome to the python program")
