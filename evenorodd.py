a896f89efaf406f9878255ea61b6d5dee224ea51
keshav <kashyapkeshav@gmail.com>
2025-06-29 14:21:45 +0530

    Add files via upload

---
+++ evenorodd.py
@@ -0,0 +1,8 @@
+num=int(input("Please enter the number\n"))
+if(num>0 & num%2==0):
+     print(num,"is an even number")
+elif(num%2!=0):
+     print(num,"is an odd number")
+else:
+     print("The number is a Prime number")
+
---
+++ sum1 to50.py
@@ -0,0 +1,5 @@
+num=int(input("enter the starting range of number\n"))
+sum=0
+for i in range(num,50):
+    sum=i+sum
+print(sum)
